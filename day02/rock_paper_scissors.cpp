00565c8daf649ccb0d6d378f2aae8a5c39b4da0f
Nick Gerrets <ngerrets@student.codam.nl>
2022-12-14 14:20:58 +0100

    improvements to day02

--- day02/rock_paper_scissors.cpp
+++ day02/rock_paper_scissors.cpp
@@ -2,79 +2,110 @@
 #include <fstream>
 #include <vector>
 
-enum Shape : int
-{
-	Rock = 1,
-	Paper = 2,
-	Scissors = 3
-};
-
 class Match
 {
 	public:
-	Shape	left;
-	Shape	right;
 
-	int	get_score_shape(void)
+	enum Shape : int
 	{
-		return ((int)right);
+		Rock = 1,
+		Paper = 2,
+		Scissors = 3
+	};
+
+	enum Condition : int
+	{
+		Lose = 1,
+		Draw = 2,
+		Win = 3
+	};
+
+	Match(Shape left, Shape right, Condition condition)
+	: left(left), right(right), con(condition) {}
+
+	void	set_from_condition(void)
+	{
+		right = Rock;
+		switch (con)
+		{
+		case Lose:
+			if (left == Rock)
+				right = Scissors;
+			else if (left == Scissors)
+				right = Paper;
+			break;
+		case Draw:
+			right = left;
+			break;
+		case Win:
+			if (left == Rock)
+				right = Paper;
+			else if (left == Paper)
+				right = Scissors;
+			break;
+		default:
+			break;
+		}
 	}
 
-	int	get_score_match(void)
+	int	get_score(void) const
 	{
+		int score = (int)right;
 		if (left == right)
-			return (3);
-		if ((left == Rock && right == Paper)
+			score += 3;
+		else if ((left == Rock && right == Paper)
 			|| (left == Paper && right == Scissors)
 			|| (left == Scissors && right == Rock))
-			return (6);
-		return (0);
+			score += 6;
+		return (score);
 	}
 
-	int	get_score(void)
-	{
-		return get_score_shape() + get_score_match();
-	}
+	private:
+
+	Shape		left;
+	Shape		right;
+	Condition	con;
 };
 
-std::vector<Match>	parse_matches(std::ifstream& input)
+std::vector<Match> parse_matches(std::istream& stream)
 {
-	std::vector<Match>	matches;
-
-	while (!input.eof())
+	std::vector<Match> matches;
+	for (std::string line; std::getline(stream, line); )
 	{
-		std::string	line;
-		std::getline(input, line);
-		if (line.length() != 3)
-			break ;
-		Match m;
-		m.left = static_cast<Shape>(Rock + line[0] - 'A');
-		m.right = static_cast<Shape>(Rock + line[2] - 'X');
-		matches.push_back(m);
+		matches.emplace_back(
+			static_cast<Match::Shape>(Match::Rock + line[0] - 'A'),
+			static_cast<Match::Shape>(Match::Rock + line[2] - 'X'),
+			static_cast<Match::Condition>(Match::Lose + line[2] - 'X')
+		);
 	}
 	return (matches);
 }
 
 int	main(int argc, char **argv)
 {
-	if (argc != 2)
+	if (argc < 2)
 		return (EXIT_FAILURE);
 	
-	std::ifstream	file;
-	file.open(argv[1]);
-
+	std::ifstream file(argv[1]);
 	if (!file)
 		return (EXIT_FAILURE);
 
+	//	Parsing
 	std::vector<Match> matches = parse_matches(file);
+
+	//	part 1
 	int	total = 0;
+	for (Match const& m : matches)
+		total += m.get_score();
+	std::cout << "Total match score (Part 1): " << total << std::endl;
+
+	total = 0;
 	for (Match& m : matches)
 	{
+		m.set_from_condition();
 		total += m.get_score();
 	}
-	
-	std::cout << "Total matches: " << matches.size() << std::endl;
-	std::cout << "Total match score: " << total << std::endl;
+	std::cout << "Total match score (Part 2): " << total << std::endl;
 
 	return (EXIT_SUCCESS);
 }
