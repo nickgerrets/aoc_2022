4759f28754d5ee3f3a41e376cf005bbc76e61c39
Nick Gerrets <ngerrets@student.codam.nl>
2022-12-14 14:08:04 +0100

    updated day01

--- day01/find_most_calories.cpp
+++ day01/find_most_calories.cpp
@@ -2,38 +2,51 @@
 #include <fstream>
 #include <vector>
 
-std::vector<int>	get_calorie_sums(std::ifstream& input)
+std::vector<int> get_calorie_sums(std::istream& stream)
 {
 	std::vector<int>	v;
 
 	int	calories = 0;
-	while (!input.eof())
+	for (std::string line; std::getline(stream, line); )
 	{
-		std::string	str;
-		std::getline(input, str);
-		if (str.length() == 0)
+		if (line.length() == 0)
 		{
 			v.push_back(calories);
 			calories = 0;
 			continue ;
 		}
-		calories += std::stoi(str);
+		calories += std::stoi(line);
 	}
 	return (v);
 }
 
 int	main(int argc, char **argv)
 {
-	std::ifstream		input;
-	
-	if (argc != 2)
+	if (argc < 2)
 		return (EXIT_FAILURE);
 
-	input.open(argv[1]);
-	std::vector<int> calorie_vector = get_calorie_sums(input);
+	std::ifstream file(argv[1]);
+	if (!file)
+		return (EXIT_FAILURE);
 
-	int max = *max_element(calorie_vector.begin(), calorie_vector.end());
-	std::cout << "Highest calorie amount: " << max << std::endl;
+	std::vector<int> cal_sums = get_calorie_sums(file);
+
+	int	sum = 0;
+	auto it = max_element(cal_sums.begin(), cal_sums.end());
+	std::cout << "Top Highest calorie amount: " << *it << std::endl;
+	sum += *it;
+	cal_sums.erase(it);
+
+	it = max_element(cal_sums.begin(), cal_sums.end());
+	std::cout << "Second Highest calorie amount: " << *it << std::endl;
+	sum += *it;
+	cal_sums.erase(it);
+
+	it = max_element(cal_sums.begin(), cal_sums.end());
+	std::cout << "Third Highest calorie amount: " << *it << std::endl;
+	sum += *it;
+
+	std::cout << "Sum: " << sum << std::endl;
 
 	return (EXIT_SUCCESS);
 }
