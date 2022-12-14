c33815b43fedab3eac8faeaaaf1dc657dc854b49
Nick Gerrets <ngerrets@student.codam.nl>
2022-12-14 17:10:07 +0100

    improvements day10

--- day10/cathode_ray_tube.cpp
+++ day10/cathode_ray_tube.cpp
@@ -9,21 +9,16 @@ struct Instruction
 	int cycles;
 };
 
-std::vector<Instruction> parse_instructions(std::ifstream& stream)
+std::vector<Instruction> parse_instructions(std::istream& stream)
 {
 	std::vector<Instruction> instructions;
-
-	while (!stream.eof())
+	for (std::string line; std::getline(stream, line); )
 	{
-		std::string line;
-		std::getline(stream, line);
-		if (line.length() == 0)
-			break ;
-		Instruction i;
-		switch (line[0])
+		Instruction i {0, 1};
+		if (line[0] == 'a')
 		{
-			case 'a': i.cycles = 2; i.value = std::stoi(line.substr(line.find_first_of("-0123456789"))); break;
-			case 'n': i.cycles = 1; i.value = 0; break;
+			i.value = std::stoi(line.substr(line.find_first_of("-0123456789")));
+			i.cycles = 2;
 		}
 		instructions.push_back(i);
 	}
@@ -37,7 +32,7 @@ void draw_display(std::array<bool, _Width * _Height> const& display)
 	{
 		for (size_t x = 0; x < _Width; ++x)
 		{
-			char c = '.';
+			char c = ' ';
 			if (display[x + y * _Width])
 				c = '#';
 			std::cout << c;
@@ -51,14 +46,13 @@ int	main(int argc, char **argv)
 	if (argc < 2)
 		return (EXIT_FAILURE);
 	
-	std::ifstream	file;
-	file.open(argv[1]);
-
+	std::ifstream file(argv[1]);
 	if (!file)
 		return (EXIT_FAILURE);
 
+	//	Parsing
 	std::vector<Instruction> instructions = parse_instructions(file);
-	std::array<bool, 40 * 6> display {0};
+	std::array<bool, 40 * 6> display {false};
 
 	size_t i = 1;
 	int regx = 1;
@@ -72,7 +66,7 @@ int	main(int argc, char **argv)
 			if (x >= regx - 1 && x <= regx + 1)
 				display[i - 1] = true;
 			//	Update signal
-			if (i == 20 || i == 60 || i == 100 || i == 140 || i == 180 || i == 220)
+			if (i == 20 || i % 40 == 20)
 				signal_strength += i * regx;
 			++i;
 		}
